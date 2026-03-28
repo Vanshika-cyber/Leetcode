24a546413fd25dcc8278df6258ff36188d2db3b4
vanshika_jaiswal13 <jaiswalvanshika624@gmail.com>
2026-03-28 13:37:27 +0530

    Time: 8 ms (51.43%), Space: 178.6 MB (20%) - LeetHub

---
+++ 2573-find-the-string-with-lcp/2573-find-the-string-with-lcp.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String findTheString(int[][] lcp) {
+        int n = lcp.length;
+        for (int i = 0; i < n; i++) {
+            if (lcp[i][i] != n - i) {
+                return "";
+            }
+        }
+        char[] word = new char[n];
+        Arrays.fill(word, '#');
+
+        char ch = 'a';
+
+        for (int i = 0; i < n; i++) {
+
+            if (word[i] == '#') {
+                if (ch > 'z') return "";
+                word[i] = ch++;
+            }
+
+            for (int j = i + 1; j < n; j++) {
+                if (lcp[i][j] > 0) {
+                    word[j] = word[i];
+                }
+            }
+        }
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (word[i] == word[j]) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+
+                if (dp[i][j] != lcp[i][j]) {
+                    return "";
+                }
+            }
+        }
+
+        return new String(word);
+    }
+}
